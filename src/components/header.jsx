c874988f490d6403b2234258f8f709ff56edfa7f
Aura Spejeraite <ArSpej@gmail.com>
2022-11-22 21:32:59 +0200

    defined dropdown nav. bar in the <header>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
 function Header() {
+
+  function toggleDropdown() {
+
+  }
+
   return (
     <header>
       <nav>
         <a href='#'>Lorem Ipsum</a>
         <a href='#'>Lorem Ipsum</a>
         <a href='#'>Lorem Ipsum</a>
+      </nav>
+
+      <span onClick={toggleDropdown()}>&#9776;</span>
+      <nav id='nav' className='dropdown'>
+        <a href="#"></a>
+        <a href="#"></a>
+        <a href="#"></a>
       </nav>
     </header>
   )
