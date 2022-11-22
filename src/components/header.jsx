ca627d0e8328fde38b6ab98005adfb5756dba43c
Aura Spejeraite <ArSpej@gmail.com>
2022-11-22 21:30:11 +0200

    defined navigation bar in the <header>

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>header</div>
+    <header>
+      <nav>
+        <a href='#'>Lorem Ipsum</a>
+        <a href='#'>Lorem Ipsum</a>
+        <a href='#'>Lorem Ipsum</a>
+      </nav>
+    </header>
   )
 }
 
