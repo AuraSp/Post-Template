e308013e5220e0a76a115484dc5efd30ff2bb8c0
Aura Spejeraite <ArSpej@gmail.com>
2022-11-22 21:40:08 +0200

    defined post card in the <main>

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function Post() {
   return (
-    <div>post</div>
+    <div className='post-card'>
+
+      {/* POST SECTION */}
+      <div className='post'>
+        <div className='post-space'>
+
+        </div>
+        <div className='post-description'>
+          <span>Lorem ipsum dolor sit amet, consectetur</span>
+          <span>Lorem ipsum</span>
+          <span>Lorem ipsum dolor sit</span>
+        </div>
+      </div>
+
+      {/* COMMENT SECTION */}
+      <div className='comment'>
+        <input
+          type="text"
+          placeholder='Placeholder...' />
+        <button
+          type='button'>submit</button>
+        <div className='comment-space'>
+
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
