3dc7de18ad63b65b983c99a2103498b36664e215
Aura Spejeraite <ArSpej@gmail.com>
2022-11-26 01:23:01 +0200

    constructed toggle nav. dropdown

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 function Header() {
 
+  const [toggle, setToggle] = useState(false);
+  const header = useRef(null);
+
+
   function toggleDropdown() {
+    setToggle(!toggle)
+
+
+      if (!toggle) {
+        header.current.classList.add('dropdown-header')
+      } else {
+        header.current.classList.remove('dropdown-header')
+      }
 
   }
 
   return (
-    <header>
+    <header ref={header} >
       <nav>
         <a href='#'>Lorem ipsum</a>
         <a href='#'>Lorem ipsum</a>
         <a href='#'>Lorem ipsum</a>
       </nav>
 
-      <span onClick={toggleDropdown()}>&#9776;</span>
-      <nav id='nav' className='dropdown'>
-        <a href="#"></a>
-        <a href="#"></a>
-        <a href="#"></a>
+      <span onClick={toggleDropdown}>&#9776;</span>
+      <nav id='nav' className={!toggle ? 'dropdown-close' : 'dropdown-open'}>
+        <a href='#'>Lorem ipsum</a>
+        <a href='#'>Lorem ipsum</a>
+        <a href='#'>Lorem ipsum</a>
       </nav>
     </header>
+
   )
 }
 
